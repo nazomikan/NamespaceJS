e682e9269d2dbd39e4d827b16eac9d2d58c4f35b
nazomikan <nazomikan@gmail.com>
2013-02-11 22:04:55 +0900

    initalize commit

---
+++ namespace.js
@@ -0,0 +1,91 @@
+(function (name, global, definition) {
+  if (typeof module !== 'undefined') {
+    module.exports = definition();
+  } else if (typeof require !== 'undefined' && typeof require.amd === 'object') {
+    define(definition);
+  } else {
+    global[name] = definition();
+  }
+})('Namespace', this, function () {
+  var arrayProto = Array.prototype
+    , slice = arrayProto.slice
+    , global = this
+    , local = {}
+    ;
+
+  function Namespace(namespaceStr) {
+    var spaces = namespaceStr.split('.')
+      , name = spaces.pop()
+      ;
+
+    this.namespace = local.createNamespace(spaces);
+    this.name = name;
+  }
+
+  Namespace.create = function () {
+    var namespace = createObject(Namespace.prototype)
+      , args = slice.call(arguments);
+
+    Namespace.apply(namespace, args);
+    return namespace;
+  };
+
+  Namespace.prototype.means = function (entity) {
+    var namespace = this.namespace
+      , name = this.name
+      ;
+
+    namespace[name] = entity;
+  };
+
+  local.createNamespace = function (spaces) {
+    var obj = global
+      , space
+      , i
+      , l
+      ;
+
+    for (i = 0, l = spaces.length; i < l; i++) {
+      space = spaces[i];
+      if (isPrimitive(obj[space])) {
+        throw new Error('namespace ' + spaces.join('.') + ' already exist and '+ space +' is primitive');
+      }
+      obj[space] = (obj[space] == null) ? {} : obj[space];
+      obj = obj[space];
+    }
+
+    return obj;
+  };
+
+  function createObject(obj) {
+    if (Object.create) {
+      return Object.create(obj);
+    }
+
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = obj;
+    return new F();
+  }
+
+  function isPrimitive(value) {
+    var result
+      ;
+
+    switch(typeof value) {
+      case 'string':
+      case 'number':
+      case 'boolean':
+        result = true;
+        break;
+      default:
+        result = false;
+    }
+
+    return result;
+  }
+
+  return Namespace;
+});
---
+++ test/namespace-test.js
@@ -0,0 +1,148 @@
+var assert = require('assert')
+  , Namespace = require('../namespace.js')
+  ;
+
+describe('Namespace', function () {
+  describe('#new', function () {
+    it('should create namespace under the global', function () {
+      var namespace = new Namespace('aaa.bbb.ccc')
+        , exist_aaa = global.hasOwnProperty('aaa')
+        , exist_aaa_bbb = aaa.hasOwnProperty('bbb')
+        , exist_aaa_bbb_ccc = aaa.bbb.hasOwnProperty('ccc')
+        , lastSpace = aaa.bbb
+        ;
+
+      assert(exist_aaa);
+      assert(exist_aaa_bbb);
+      assert.equal(false, exist_aaa_bbb_ccc);
+    });
+
+    it('should not create namespace when spacestr is not chaining', function () {
+      var namespace = new Namespace('aaa')
+        , exist_aaa = global.hasOwnProperty('aaa')
+        ;
+
+      assert.equal(false, exist_aaa);
+    });
+
+    it('should reuse namespace when namespace already exists and it is object', function () {
+      var exist_aaa_bbb
+        ;
+
+      global.aaa = {ccc: 'hoge'};
+      new Namespace('aaa.bbb.ccc');
+      exist_aaa_bbb = aaa.hasOwnProperty('bbb');
+
+      assert.equal(true, exist_aaa_bbb);
+      assert.equal('hoge', aaa.ccc);
+    });
+
+    it('should raise err when namespace already exists and it is string(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = 'aaa';
+      assert.throws(function () {
+        new Namespace('aaa.bbb');
+      }, Error, message);
+    });
+
+    it('should raise err when namespace already exists and it is number(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = 123;
+      assert.throws(function () {
+        new Namespace('aaa.bbb');
+      }, Error, message);
+    });
+
+    it('should raise err when namespace already exists, it is boolean(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = true;
+      assert.throws(function () {
+        new Namespace('aaa.bbb');
+      }, Error, message);
+    });
+  });
+
+  describe('#create', function () {
+    it('should create namespace under the global', function () {
+      var namespace = Namespace.create('aaa.bbb.ccc')
+        , exist_aaa = global.hasOwnProperty('aaa')
+        , exist_aaa_bbb = aaa.hasOwnProperty('bbb')
+        , exist_aaa_bbb_ccc = aaa.bbb.hasOwnProperty('ccc')
+        ;
+
+      assert(exist_aaa);
+      assert(exist_aaa_bbb);
+      assert.equal(false, exist_aaa_bbb_ccc);
+    });
+
+    it('should not create namespace when spacestr is not chaining', function () {
+      var namespace = Namespace.create('aaa')
+        , exist_aaa = global.hasOwnProperty('aaa')
+        ;
+
+      assert.equal(false, exist_aaa);
+    });
+
+    it('should reuse namespace when namespace already exists and it is object', function () {
+      var exist_aaa_bbb
+        ;
+
+      global.aaa = {ccc: 'hoge'};
+      Namespace.create('aaa.bbb.ccc');
+      exist_aaa_bbb = aaa.hasOwnProperty('bbb');
+
+      assert.equal(true, exist_aaa_bbb);
+      assert.equal('hoge', aaa.ccc);
+    });
+
+    it('should raise err when namespace already exists, it is string(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = 'aaa';
+      assert.throws(function () {
+        Namespace.create('aaa.bbb');
+      }, Error, message);
+    });
+
+    it('should raise err when namespace already exists, it is number(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = 123;
+      assert.throws(function () {
+        Namespace.create('aaa.bbb');
+      }, Error, message);
+    });
+
+    it('should raise err when namespace already exists, it is boolean(primitive)', function () {
+      var message = 'namespace aaa already exist and aaa is primitive'
+        ;
+
+      global.aaa = true;
+      assert.throws(function () {
+        Namespace.create('aaa.bbb');
+      }, Error, message);
+    });
+  });
+
+  describe('#means', function () {
+    it('should define under the namespace', function () {
+      var obj = {}
+        ;
+
+      Namespace.create('aaa.bbb.ccc').means(obj);
+      assert.equal(obj, aaa.bbb.ccc);
+    });
+  });
+
+  afterEach(function () {
+    delete global.aaa;
+  });
+});
